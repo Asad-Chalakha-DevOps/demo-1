bc24d735d178df4d280257ca850161530be9165e
Asad-Chalakha-DevOps <asad.c@peerbits.com>
2025-10-15 16:11:45 +0530

    demo-final

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", apiRoutes);
+// Mount all routes at root
+app.use("/", apiRoutes);
 
 app.listen(5000, "0.0.0.0", () => {
   console.log("Backend running at http://0.0.0.0:5000");
 });
-
--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const faker = require("faker"); // <-- change here
+const faker = require("faker");
 
 // Generate fake user data
-router.get("/users", (req, res) => {
+router.get("/", (req, res) => {
   let users = [];
   for (let i = 0; i < 1000; i++) {
     users.push({
       id: i + 1,
-      name: faker.name.findName(),        // <-- change here
-      email: faker.internet.email(),      // same
-      city: faker.address.city()          // <-- change here
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      city: faker.address.city()
     });
   }
   res.json(users);
